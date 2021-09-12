8ed9f021ac3bd0c73f72c8d61c84038169d0b48d
kkojae <jaejeung3210@gmail.com>
2021-09-12 21:04:49 +0900

    create onClickDaysEls & modify prettier printWidth: 120

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,8 +1,6 @@
 // todo-count
 function setTodoCount(day: string): void {
-  const itemLength: number = document.querySelectorAll(
-    `.${day}-container .weekly-items .weekly-item`,
-  ).length;
+  const itemLength: number = document.querySelectorAll(`.${day}-container .weekly-items .weekly-item`).length;
 
   const todoCountEl: Element | null = document.querySelector(
     `.${day}-container .weekly-container-header .weekly-todo-count`,
@@ -13,22 +11,28 @@ function setTodoCount(day: string): void {
   }
 }
 
-function makeAddModal(weeklyEl: Element) {
-  // const [monIcon, tueIcon, wedIcon, thuIcon, friIcon]: Element[] =
-  //   document.querySelectorAll('.add-icon');
-  // weeklyEl.addEventListener('click', event => {
-  //   if (event.target === monIcon) {
-  //     console.log('mon');
-  //   } else if (event.target === tueIcon) {
-  //     console.log('tue');
-  //   } else if (event.target === wedIcon) {
-  //     console.log('wed');
-  //   } else if (event.target === thuIcon) {
-  //     console.log('thu');
-  //   } else if (event.target === friIcon) {
-  //     console.log('fri');
-  //   }
-  // });
+function onClickPlusBtn(weeklyEl: Element, plusEl: Element): void {
+  const [monPlusIcon, tuePlusIcon, wedPlusIcon, thuPlusIcon, friPlusIcon]: Element[] =
+    document.querySelectorAll('.add-icon');
+
+  weeklyEl.addEventListener('click', event => {
+    if (event.target === monPlusIcon) {
+      plusEl.classList.add('active');
+      console.log('mon');
+    } else if (event.target === tuePlusIcon) {
+      plusEl.classList.add('active');
+      console.log('tue');
+    } else if (event.target === wedPlusIcon) {
+      plusEl.classList.add('active');
+      console.log('wed');
+    } else if (event.target === thuPlusIcon) {
+      plusEl.classList.add('active');
+      console.log('thu');
+    } else if (event.target === friPlusIcon) {
+      plusEl.classList.add('active');
+      console.log('fri');
+    }
+  });
 }
 
 function onClickCancelBtn(plusEl: Element): void {
@@ -38,17 +42,61 @@ function onClickCancelBtn(plusEl: Element): void {
   });
 }
 
+function hasClassListActive(Els: Element[]): [boolean, Element] {
+  let bool = false;
+  let trueElement: Element;
+  Els.forEach(El => {
+    if (El.classList.contains('active')) {
+      bool = true;
+      trueElement = El;
+    }
+  });
+  return [bool, trueElement];
+}
+
+function checkEls(defaultEl: Element, anotherEls: Element[]) {
+  const [checkBoolean, checkEl]: [boolean, Element] = hasClassListActive(anotherEls);
+  if (!checkBoolean) {
+    defaultEl.classList.add('active');
+  } else {
+    checkEl.classList.remove('active');
+    defaultEl.classList.add('active');
+  }
+}
+
+function onClickDaysEls(plusEl: Element): void {
+  const [mondayItemEl, tuesdayItemEl, wednesdayItemEl, thursdayItemEl, fridayItemEl]: Element[] =
+    document.querySelectorAll('.plus-day-item');
+
+  plusEl.addEventListener('click', event => {
+    if (event.target === mondayItemEl) {
+      checkEls(mondayItemEl, [tuesdayItemEl, wednesdayItemEl, thursdayItemEl, fridayItemEl]);
+    } else if (event.target === tuesdayItemEl) {
+      checkEls(tuesdayItemEl, [mondayItemEl, wednesdayItemEl, thursdayItemEl, fridayItemEl]);
+    } else if (event.target === wednesdayItemEl) {
+      checkEls(wednesdayItemEl, [mondayItemEl, tuesdayItemEl, thursdayItemEl, fridayItemEl]);
+    } else if (event.target === thursdayItemEl) {
+      checkEls(thursdayItemEl, [mondayItemEl, tuesdayItemEl, wednesdayItemEl, fridayItemEl]);
+    } else if (event.target === fridayItemEl) {
+      checkEls(fridayItemEl, [mondayItemEl, tuesdayItemEl, thursdayItemEl, wednesdayItemEl]);
+    }
+  });
+}
+
 function main(): void {
   const dayList: string[] = ['mon', 'tue', 'wed', 'thu', 'fri'];
   dayList.forEach(day => setTodoCount(day));
-  const weeklyEl: Element | null = document.querySelector('.weekly-section');
-  // if (weeklyEl) {
-  //   makeAddModal(weeklyEl);
-  // }
 
+  const weeklyEl: Element | null = document.querySelector('.weekly-section');
   const plusEl: Element | null = document.querySelector('.plus-section');
+
+  if (weeklyEl && plusEl) {
+    onClickPlusBtn(weeklyEl, plusEl);
+  }
+
   if (plusEl) {
     onClickCancelBtn(plusEl);
+    onClickDaysEls(plusEl);
   }
 }
 
