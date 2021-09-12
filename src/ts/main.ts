fdd0a0a71ddb65b2fd6ad35cd4753149a56620b3
kkojae <jaejeung3210@gmail.com>
2021-09-12 20:04:16 +0900

    create onClickCancelBtn event

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -13,9 +13,43 @@ function setTodoCount(day: string): void {
   }
 }
 
+function makeAddModal(weeklyEl: Element) {
+  // const [monIcon, tueIcon, wedIcon, thuIcon, friIcon]: Element[] =
+  //   document.querySelectorAll('.add-icon');
+  // weeklyEl.addEventListener('click', event => {
+  //   if (event.target === monIcon) {
+  //     console.log('mon');
+  //   } else if (event.target === tueIcon) {
+  //     console.log('tue');
+  //   } else if (event.target === wedIcon) {
+  //     console.log('wed');
+  //   } else if (event.target === thuIcon) {
+  //     console.log('thu');
+  //   } else if (event.target === friIcon) {
+  //     console.log('fri');
+  //   }
+  // });
+}
+
+function onClickCancelBtn(plusEl: Element): void {
+  const cancelBtnEl: Element | null = document.querySelector('.btn__cancel');
+  cancelBtnEl?.addEventListener('click', () => {
+    plusEl.classList.remove('active');
+  });
+}
+
 function main(): void {
   const dayList: string[] = ['mon', 'tue', 'wed', 'thu', 'fri'];
   dayList.forEach(day => setTodoCount(day));
+  const weeklyEl: Element | null = document.querySelector('.weekly-section');
+  // if (weeklyEl) {
+  //   makeAddModal(weeklyEl);
+  // }
+
+  const plusEl: Element | null = document.querySelector('.plus-section');
+  if (plusEl) {
+    onClickCancelBtn(plusEl);
+  }
 }
 
 main();
