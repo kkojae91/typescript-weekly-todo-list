219661c0897f8cf1fb4fb66e8a1fc5e73fedfede
kkojae91 <jaejeung3210@gmail.com>
2021-09-21 23:40:09 +0900

    onClickCheckIcon localStorage저장 수정

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -568,23 +568,29 @@ function onClickCheckIcon(weeklyEl: Element): void {
         const currentWeeklyItemEl = document.querySelector(`.weekly-item[data-itemid="${currentId}"]`);
         const editIconEl = document.querySelector(`.edit-icon[data-editid="${currentId}"]`);
         let template = '';
+        let todoList: [] | null = JSON.parse(localStorage.getItem('todo-list'));
         if (currentWeeklyItemEl?.classList.contains('active')) {
           currentWeeklyItemEl.classList.remove('active');
           editIconEl?.classList.remove('active');
           template = currentWeeklyItemEl?.innerHTML;
+          todoList?.forEach(todo => {
+            if (String(todo.randomId) === currentId) {
+              todo.template = template;
+              todo.isActive = false;
+            }
+          });
         } else {
           currentWeeklyItemEl?.classList.add('active');
           editIconEl?.classList.add('active');
           template = currentWeeklyItemEl?.innerHTML;
+          todoList?.forEach(todo => {
+            if (String(todo.randomId) === currentId) {
+              todo.template = template;
+              todo.isActive = true;
+            }
+          });
         }
 
-        let todoList: [] | null = JSON.parse(localStorage.getItem('todo-list'));
-        todoList?.forEach(todo => {
-          if (String(todo.randomId) === currentId) {
-            todo.template = template;
-            todo.isActive = true;
-          }
-        });
         localStorage.setItem('todo-list', JSON.stringify(todoList));
 
         const currentDay: string =
