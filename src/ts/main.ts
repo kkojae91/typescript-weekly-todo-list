d22c605888e960c98556d3a7fdf1869469053fa0
kkojae91 <jaejeung3210@gmail.com>
2021-09-22 18:26:06 +0900

    onClickEditBtn 파일 분리

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -4,6 +4,7 @@ import getDayOfEdit from './get-day-of-edit';
 import importPreviousRecord from './import-previous-record';
 import onClickDarkAndLightModeIcon from './project-theme';
 import onClickCheckIcon from './event/onClick-check-icon';
+import onClickEditBtn from './event/onClick-edit-btn';
 
 function initializeClassListActive(Els: Element[]): void {
   Els.forEach(El => {
@@ -383,75 +384,6 @@ function onClickEditIcon(weeklyEl: Element) {
         editBtn.classList.add('active');
       }
     });
-  });
-}
-
-function onClickEditBtn(plusEl: Element): void {
-  const editBtn: Element | null = document.querySelector('.btn__edit');
-  const completionBtn: Element = document.querySelector('.btn__completion');
-  plusEl.addEventListener('click', event => {
-    if (event.target === editBtn) {
-      const currentId: string | null = localStorage.getItem('currentId');
-      const currentDay: string | null = localStorage.getItem('currentDay');
-
-      const currentPlusTitleEl = document.querySelector('.plus-title-input');
-      const currentPlusTimeEl = document.querySelector('#time-input');
-      const currentPlusTitleInput: string = currentPlusTitleEl?.value;
-      const currentPlusTimeInput: string = currentPlusTimeEl?.value;
-      const currentPlusDayItemText: string | null | undefined =
-        document.querySelector('.plus-day-item.active')?.textContent;
-      const currentPlusImportantStarCount: string =
-        document.querySelector('.important-item-star.active')?.dataset.important;
-      const editWeeklyItemEl: Element = document.querySelector(`.weekly-item[data-itemid="${currentId}"]`);
-
-      const plusDayEls = document.querySelectorAll('.plus-day-item');
-      const plusImportantStarEls: Element[] = document.querySelectorAll('.important-item-star');
-
-      if (currentDay === currentPlusDayItemText) {
-        const template: string = makeWeeklyIteminnerHTML(
-          currentId,
-          currentPlusTitleInput,
-          currentPlusTimeInput,
-          currentPlusImportantStarCount,
-        );
-        editWeeklyItemEl.innerHTML = template;
-
-        const dayList: string[] = ['mon', 'tue', 'wed', 'thu', 'fri'];
-        dayList.forEach(day => setTodoCount(day));
-
-        let todoList: [] | null = JSON.parse(localStorage.getItem('todo-list'));
-        todoList?.forEach(todo => {
-          if (String(todo.randomId) === currentId) {
-            todo.template = template;
-          }
-        });
-        localStorage.setItem('todo-list', JSON.stringify(todoList));
-      } else {
-        const weeklyItemEl: Element = document.querySelector(`.weekly-item[data-itemid="${currentId}"]`);
-        makeWeeklyItem(
-          currentPlusTitleInput,
-          currentPlusTimeInput,
-          currentPlusDayItemText,
-          currentPlusImportantStarCount,
-        );
-        weeklyItemEl.remove();
-
-        const dayList: string[] = ['mon', 'tue', 'wed', 'thu', 'fri'];
-        dayList.forEach(day => setTodoCount(day));
-
-        let todoList: [] | null = JSON.parse(localStorage.getItem('todo-list'));
-
-        todoList = todoList?.filter(todo => String(todo.randomId) !== currentId);
-        localStorage.setItem('todo-list', JSON.stringify(todoList));
-      }
-      completionBtn.classList.remove('active');
-      editBtn.classList.add('active');
-      plusEl.classList.remove('active');
-      initializeClassListActive(plusDayEls);
-      initializeImportantStarEls(plusImportantStarEls);
-      currentPlusTitleEl.value = '';
-      currentPlusTimeEl.value = '';
-    }
   });
 }
 
