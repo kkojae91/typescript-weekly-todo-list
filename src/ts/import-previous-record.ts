977fc0724e1f26d9078886742813aba677857e6d
kkojae91 <jaejeung3210@gmail.com>
2021-09-22 21:48:20 +0900

    import-previous-record.ts 타입 방어 코드 작성

--- src/ts/import-previous-record.ts
+++ src/ts/import-previous-record.ts
@@ -1,32 +1,47 @@
 import setTodoCount from './set-todo-count';
 import dragAndDrop from './drag-and-drop';
 
+interface IpreviousRecord {
+  randomId: number;
+  day: string;
+  template: string;
+  isActive: boolean;
+}
+
 export default function importPreviousRecord(): void {
-  const previousRecord = JSON.parse(localStorage.getItem('todo-list'));
+  const todoListWithString: string | null = localStorage.getItem('todo-list');
+  let previousRecord: IpreviousRecord[] | null = null;
+  if (todoListWithString) {
+    previousRecord = JSON.parse(todoListWithString);
+  }
 
-  for (let i = 0; i < previousRecord.length; i++) {
-    const id = previousRecord[i].randomId;
-    const day = previousRecord[i].day;
-    const template = previousRecord[i].template;
-    const isActive = previousRecord[i].isActive;
-    const weeklyItemEl: Element = document.createElement('div');
-    weeklyItemEl.setAttribute('class', 'weekly-item');
-    weeklyItemEl.setAttribute('data-itemid', String(id));
-    weeklyItemEl.setAttribute('draggable', 'true');
-    if (isActive) {
-      weeklyItemEl.setAttribute('class', 'weekly-item active');
-    }
-
-    weeklyItemEl.innerHTML = template;
-
-    const weeklyContainerEls: Elemnet[] = document.querySelectorAll('.weekly-container');
-    weeklyContainerEls.forEach(weeklyContainerEl => {
-      if (weeklyContainerEl.dataset.weekly === day) {
-        weeklyContainerEl.lastChild.previousSibling.appendChild(weeklyItemEl);
+  if (previousRecord) {
+    for (let i = 0; i < previousRecord.length; i++) {
+      const id: number = previousRecord[i].randomId;
+      const day: string = previousRecord[i].day;
+      const template: string = previousRecord[i].template;
+      const isActive: boolean = previousRecord[i].isActive;
+      const weeklyItemEl: HTMLDivElement = document.createElement('div');
+      weeklyItemEl.setAttribute('class', 'weekly-item');
+      weeklyItemEl.setAttribute('data-itemid', String(id));
+      weeklyItemEl.setAttribute('draggable', 'true');
+      if (isActive) {
+        weeklyItemEl.setAttribute('class', 'weekly-item active');
       }
-    });
-    const dayList: string[] = ['mon', 'tue', 'wed', 'thu', 'fri'];
-    dayList.forEach(day => setTodoCount(day));
+
+      weeklyItemEl.innerHTML = template;
+
+      const weeklyContainerEls: NodeListOf<HTMLDivElement> =
+        document.querySelectorAll<HTMLDivElement>('.weekly-container');
+
+      weeklyContainerEls.forEach(weeklyContainerEl => {
+        if (weeklyContainerEl.dataset.weekly === day) {
+          weeklyContainerEl?.lastChild?.previousSibling?.appendChild(weeklyItemEl);
+        }
+      });
+      const dayList: string[] = ['mon', 'tue', 'wed', 'thu', 'fri'];
+      dayList.forEach(day => setTodoCount(day));
+    }
   }
   dragAndDrop();
 }
