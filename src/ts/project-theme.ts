2bd121967614c1d69b2a3eadb2162ad734e3e434
kkojae91 <jaejeung3210@gmail.com>
2021-09-22 21:09:48 +0900

    project-theme 타입 방어 코드 작성

--- src/ts/project-theme.ts
+++ src/ts/project-theme.ts
@@ -1,17 +1,29 @@
-// porject theme
-function applyDarkMode(darkIconEl: Element, lightIconEl: Element, htmlEl: HTMLHtmlElement) {
+function applyDarkMode(darkIconEl: Element, lightIconEl: Element, htmlEl: HTMLHtmlElement): void {
   darkIconEl?.classList.remove('active');
   lightIconEl?.classList.add('active');
   htmlEl?.classList.add('dark');
 }
 
-function applyLightMode(darkIconEl: Element, lightIconEl: Element, htmlEl: HTMLHtmlElement) {
+function applyLightMode(darkIconEl: Element, lightIconEl: Element, htmlEl: HTMLHtmlElement): void {
   darkIconEl?.classList.add('active');
   lightIconEl?.classList.remove('active');
   htmlEl?.classList.remove('dark');
 }
 
-export default function onClickDarkAndLightModeIcon(toggleEl: Element) {
+function previousProjectThemeCheck(
+  projectTheme: string,
+  darkIconEl: Element,
+  lightIconEl: Element,
+  htmlEl: HTMLHtmlElement,
+): void {
+  if (projectTheme === 'dark' && darkIconEl && lightIconEl && htmlEl) {
+    applyDarkMode(darkIconEl, lightIconEl, htmlEl);
+  } else if (darkIconEl && lightIconEl && htmlEl) {
+    applyLightMode(darkIconEl, lightIconEl, htmlEl);
+  }
+}
+
+export default function onClickDarkAndLightModeIcon(toggleEl: Element): void {
   const htmlEl: HTMLHtmlElement | null = document.querySelector('html');
   const darkIconEl: Element | null = document.querySelector('.dark-mode');
   const lightIconEl: Element | null = document.querySelector('.light-mode');
@@ -19,20 +31,27 @@ export default function onClickDarkAndLightModeIcon(toggleEl: Element) {
   const darkToggle: Element | null = document.querySelector('.dark-toggle');
   const lightIcon: Element | null = document.querySelector('.light-icon');
   const lightToggle: Element | null = document.querySelector('.light-toggle');
-
   const projectTheme: string | null = localStorage.getItem('theme');
 
-  if (projectTheme === 'dark') {
-    applyDarkMode(darkIconEl, lightIconEl, htmlEl);
-  } else {
-    applyLightMode(darkIconEl, lightIconEl, htmlEl);
+  if (projectTheme && darkIconEl && lightIconEl && htmlEl) {
+    previousProjectThemeCheck(projectTheme, darkIconEl, lightIconEl, htmlEl);
   }
 
   toggleEl.addEventListener('click', event => {
-    if (event.target === darkIconEl || event.target === darkIcon || event.target === darkToggle) {
+    if (
+      (event.target === darkIconEl || event.target === darkIcon || event.target === darkToggle) &&
+      darkIconEl &&
+      lightIconEl &&
+      htmlEl
+    ) {
       localStorage.setItem('theme', 'dark');
       applyDarkMode(darkIconEl, lightIconEl, htmlEl);
-    } else if (event.target === lightIconEl || event.target === lightIcon || event.target === lightToggle) {
+    } else if (
+      (event.target === lightIconEl || event.target === lightIcon || event.target === lightToggle) &&
+      darkIconEl &&
+      lightIconEl &&
+      htmlEl
+    ) {
       localStorage.setItem('theme', 'light');
       applyLightMode(darkIconEl, lightIconEl, htmlEl);
     }
