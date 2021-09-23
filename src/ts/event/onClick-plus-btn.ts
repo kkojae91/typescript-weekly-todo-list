51a3dac9dbf8d27e7913a74d251b617c00b877c8
kkojae91 <jaejeung3210@gmail.com>
2021-09-23 14:14:04 +0900

    onClick-plus-btn.ts 타입 방어 코드 작성

--- src/ts/event/onClick-plus-btn.ts
+++ src/ts/event/onClick-plus-btn.ts
@@ -6,25 +6,25 @@ function addAndRemoveAtive(plusEl: Element, itemEl: Element, completionBtn: Elem
 }
 
 export default function onClickPlusBtn(weeklyEl: Element, plusEl: Element): void {
-  const [monPlusIcon, tuePlusIcon, wedPlusIcon, thuPlusIcon, friPlusIcon]: Element[] =
-    document.querySelectorAll('.add-icon');
+  const [monPlusIcon, tuePlusIcon, wedPlusIcon, thuPlusIcon, friPlusIcon]: NodeListOf<HTMLDivElement> =
+    document.querySelectorAll<HTMLDivElement>('.add-icon');
 
-  const [mondayItemEl, tuesdayItemEl, wednesdayItemEl, thursdayItemEl, fridayItemEl]: Element[] =
-    document.querySelectorAll('.plus-day-item');
+  const [mondayItemEl, tuesdayItemEl, wednesdayItemEl, thursdayItemEl, fridayItemEl]: NodeListOf<HTMLDivElement> =
+    document.querySelectorAll<HTMLDivElement>('.plus-day-item');
 
-  const completionBtn: Element = document.querySelector('.btn__completion');
-  const editBtn: Element = document.querySelector('.btn__edit');
+  const completionBtn: HTMLDivElement | null = document.querySelector('.btn__completion');
+  const editBtn: HTMLDivElement | null = document.querySelector('.btn__edit');
 
   weeklyEl.addEventListener('click', event => {
-    if (event.target === monPlusIcon) {
+    if (event.target === monPlusIcon && completionBtn && editBtn) {
       addAndRemoveAtive(plusEl, mondayItemEl, completionBtn, editBtn);
-    } else if (event.target === tuePlusIcon) {
+    } else if (event.target === tuePlusIcon && completionBtn && editBtn) {
       addAndRemoveAtive(plusEl, tuesdayItemEl, completionBtn, editBtn);
-    } else if (event.target === wedPlusIcon) {
+    } else if (event.target === wedPlusIcon && completionBtn && editBtn) {
       addAndRemoveAtive(plusEl, wednesdayItemEl, completionBtn, editBtn);
-    } else if (event.target === thuPlusIcon) {
+    } else if (event.target === thuPlusIcon && completionBtn && editBtn) {
       addAndRemoveAtive(plusEl, thursdayItemEl, completionBtn, editBtn);
-    } else if (event.target === friPlusIcon) {
+    } else if (event.target === friPlusIcon && completionBtn && editBtn) {
       addAndRemoveAtive(plusEl, fridayItemEl, completionBtn, editBtn);
     }
   });
