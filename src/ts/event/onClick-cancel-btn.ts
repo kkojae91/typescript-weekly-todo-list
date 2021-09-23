0bc6605fe8248ea22d10198cf8ea195d12416ba0
kkojae91 <jaejeung3210@gmail.com>
2021-09-23 14:50:10 +0900

    onClick-cancel-btn.ts 타입 방어 코드 작성

--- src/ts/event/onClick-cancel-btn.ts
+++ src/ts/event/onClick-cancel-btn.ts
@@ -1,16 +1,19 @@
 import { initializeClassListActive, initializeImportantStarEls } from '../initialize/initialize';
 
-export default function onClickCancelBtn(plusEl: Element): void {
-  const cancelBtnEl: Element | null = document.querySelector('.btn__cancel');
-  const plusDayItems: Element[] = document.querySelectorAll('.plus-day-item');
-  const plusImportantStarEls: Element[] = document.querySelectorAll('.important-item-star');
-  const titleInputEl = <HTMLInputElement>document.querySelector('.plus-title-input');
-  const timeInputEl = <HTMLInputElement>document.querySelector('#time-input');
+export default function onClickCancelBtn(plusEl: HTMLTableSectionElement): void {
+  const cancelBtnEl: HTMLDivElement | null = document.querySelector<HTMLDivElement>('.btn__cancel');
+  const plusDayItems: NodeListOf<HTMLHeadingElement> = document.querySelectorAll<HTMLHeadingElement>('.plus-day-item');
+  const plusImportantStarEls: NodeListOf<HTMLDivElement> =
+    document.querySelectorAll<HTMLDivElement>('.important-item-star');
+  const titleInputEl: HTMLInputElement | null = document.querySelector<HTMLInputElement>('.plus-title-input');
+  const timeInputEl: HTMLInputElement | null = document.querySelector<HTMLInputElement>('#time-input');
   cancelBtnEl?.addEventListener('click', () => {
     plusEl.classList.remove('active');
     initializeClassListActive(plusDayItems);
     initializeImportantStarEls(plusImportantStarEls);
-    titleInputEl.value = '';
-    timeInputEl.value = '';
+    if (titleInputEl && timeInputEl) {
+      titleInputEl.value = '';
+      timeInputEl.value = '';
+    }
   });
 }
