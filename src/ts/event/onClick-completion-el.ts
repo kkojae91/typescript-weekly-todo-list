83c2cc8c50330bdf0e9dbd294795fe32f60cf3aa
kkojae91 <jaejeung3210@gmail.com>
2021-09-23 14:46:35 +0900

    onClick-completion-el.ts 타입 방어 코드 작성

--- src/ts/event/onClick-completion-el.ts
+++ src/ts/event/onClick-completion-el.ts
@@ -3,24 +3,26 @@ import { makeWeeklyItem } from '../template/makeTemplate';
 import { initializeClassListActive, initializeImportantStarEls } from '../initialize/initialize';
 
 function targetCheck(
-  plusEl: Element,
-  plusDayEls: Element[],
-  titleInputEl: Element,
-  timeInputEl: Element,
-  dayItemEl: Element,
-  importantItemEl: Element,
-  plusImportantStarEls: Element[],
+  plusEl: HTMLTableSectionElement,
+  plusDayEls: NodeListOf<HTMLHeadingElement>,
+  titleInputEl: HTMLInputElement,
+  timeInputEl: HTMLInputElement,
+  dayItemEl: HTMLHeadingElement,
+  importantItemEl: HTMLDivElement,
+  plusImportantStarEls: NodeListOf<HTMLDivElement>,
 ): void {
   const userTitleInput: string = titleInputEl.value;
   const userTimeInput: string = timeInputEl.value;
   const userDayInput: string | null = dayItemEl?.textContent;
-  const userImportantInput: string = importantItemEl.dataset.important;
+  const userImportantInput: string | undefined = importantItemEl.dataset.important;
   if (userTitleInput === '') {
     alert('Enter the title');
   } else if (userTimeInput === '') {
     alert('Enter the time');
   } else {
-    makeWeeklyItem(userTitleInput, userTimeInput, userDayInput, userImportantInput);
+    if (userDayInput && userImportantInput) {
+      makeWeeklyItem(userTitleInput, userTimeInput, userDayInput, userImportantInput);
+    }
     plusEl.classList.remove('active');
     initializeClassListActive(plusDayEls);
     initializeImportantStarEls(plusImportantStarEls);
@@ -29,16 +31,17 @@ function targetCheck(
   }
 }
 
-export default function onClickCompletionEl(plusEl: Element): void {
-  const plusDayEls = document.querySelectorAll('.plus-day-item');
-  const completionEl: Element | null = document.querySelector('.btn__completion');
-  const titleInputEl = <HTMLInputElement>document.querySelector('.plus-title-input');
-  const timeInputEl = <HTMLInputElement>document.querySelector('#time-input');
-  const plusImportantStarEls: Element[] = document.querySelectorAll('.important-item-star');
+export default function onClickCompletionEl(plusEl: HTMLTableSectionElement): void {
+  const plusDayEls: NodeListOf<HTMLHeadingElement> = document.querySelectorAll<HTMLHeadingElement>('.plus-day-item');
+  const completionEl: HTMLDivElement | null = document.querySelector<HTMLDivElement>('.btn__completion');
+  const titleInputEl: HTMLInputElement | null = document.querySelector<HTMLInputElement>('.plus-title-input');
+  const timeInputEl: HTMLInputElement | null = document.querySelector<HTMLInputElement>('#time-input');
+  const plusImportantStarEls: NodeListOf<HTMLDivElement> =
+    document.querySelectorAll<HTMLDivElement>('.important-item-star');
   plusEl.addEventListener('click', event => {
-    const dayItemEl: Element | null = document.querySelector('.plus-day-item.active');
-    const importantItemEl: Element | null = document.querySelector('.important-item-star.active');
-    if (event.target === completionEl) {
+    const dayItemEl = document.querySelector<HTMLHeadingElement>('.plus-day-item.active');
+    const importantItemEl = document.querySelector<HTMLDivElement>('.important-item-star.active');
+    if (event.target === completionEl && titleInputEl && timeInputEl && dayItemEl && importantItemEl) {
       targetCheck(plusEl, plusDayEls, titleInputEl, timeInputEl, dayItemEl, importantItemEl, plusImportantStarEls);
       dragAndDrop();
     }
